ed1300a4e45471b4504ea63d0e4cb06a44ce219a
geauxvirtual <justin.guidroz@gmail.com>
2015-10-14 16:42:18 -0700

    Fix import path for hana package

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "github.com/intelsdi-x/pulse/control/plugin"
-    "github.com/intelsdi-x/pulse/plugin/publisher/pulse-publisher-hana/hana"
+	"github.com/intelsdi-x/pulse-plugin-publisher-hana/hana"
+	"github.com/intelsdi-x/pulse/control/plugin"
 )
 
 func main() {
-    meta := hana.Meta()
-    plugin.Start(meta, hana.NewHANAPublisher(), os.Args[1])
+	meta := hana.Meta()
+	plugin.Start(meta, hana.NewHANAPublisher(), os.Args[1])
 }
-
